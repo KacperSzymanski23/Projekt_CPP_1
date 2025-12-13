9ed190a82f7c96d3aed3a375400570ab3644dccd
maciejstyczen <ambler-lynxes0b@icloud.com>
2025-12-13 12:09:24 +0100

    Dodano nową klasę widgetu sideBar

---
+++ src/sidebar.cpp
@@ -0,0 +1,56 @@
+#include "sidebar.h"
+#include "icons.hpp"
+
+SideBar::SideBar(QWidget *parent)
+	: QWidget(parent)
+	, m_sideBarLayout(new QVBoxLayout(this))
+	, m_settingsDialog(new SettingsDialog(this))
+	, m_showLibraryButton(new QToolButton(this))
+	, m_showPlaylistsButton(new QToolButton(this))
+	, m_showFavoriteButton(new QToolButton(this))
+	, m_showAuthorsButton(new QToolButton(this))
+	, m_showAlbumsButton(new QToolButton(this))
+	, m_settingsButton(new QToolButton(this)){
+
+	constexpr QSize ICON_SIZE = QSize(20, 20);
+
+	m_showLibraryButton->setIcon(Icons::LIBRARY);
+	m_showLibraryButton->setIconSize(ICON_SIZE);
+	m_showLibraryButton->setStatusTip(tr("Library"));
+
+	m_showPlaylistsButton->setIcon(Icons::PLAYLIST);
+	m_showPlaylistsButton->setIconSize(ICON_SIZE);
+	m_showPlaylistsButton->setStatusTip(tr("Playlists"));
+
+	m_showFavoriteButton->setIcon(Icons::FAVORITE);
+	m_showFavoriteButton->setIconSize(ICON_SIZE);
+	m_showFavoriteButton->setStatusTip(tr("Favorite"));
+
+	m_showAuthorsButton->setIcon(Icons::AUTHORS);
+	m_showAuthorsButton->setIconSize(ICON_SIZE);
+	m_showAuthorsButton->setStatusTip(tr("Authors"));
+
+	m_showAlbumsButton->setIcon(Icons::ALBUMS);
+	m_showAlbumsButton->setIconSize(ICON_SIZE);
+	m_showAlbumsButton->setStatusTip(tr("Albums"));
+
+	m_settingsButton->setIcon(Icons::SETTINGS);
+	m_settingsButton->setIconSize(ICON_SIZE);
+	m_settingsButton->setStatusTip(tr("Settings"));
+
+	connect(m_settingsButton, &QToolButton::clicked, m_settingsDialog, [this]() {
+			m_settingsDialog->show();
+			m_settingsDialog->raise();
+			m_settingsDialog->activateWindow();
+	});
+
+	m_sideBarLayout->addWidget(m_showLibraryButton);
+	m_sideBarLayout->addWidget(m_showPlaylistsButton);
+	m_sideBarLayout->addWidget(m_showFavoriteButton);
+	m_sideBarLayout->addWidget(m_showAuthorsButton);
+	m_sideBarLayout->addWidget(m_showAlbumsButton);
+
+	m_sideBarLayout->addStretch();
+
+	m_sideBarLayout->addWidget(m_settingsButton);
+}
---
+++ src/sidebar.h
@@ -0,0 +1,32 @@
+
+#ifndef SIDEBAR_HPP
+#define SIDEBAR_HPP
+
+#include "settingsdialog.hpp"
+// Qt
+#include <QWidget>
+#include <QToolButton>
+#include <QVBoxLayout>
+#include <QSpacerItem>
+
+class SideBar : public QWidget {
+	Q_OBJECT
+
+  public:
+	explicit SideBar(QWidget *parent = nullptr);
+
+private:
+	QVBoxLayout *m_sideBarLayout;  // Układ elementów GUI dla paska bocznego
+
+	SettingsDialog *m_settingsDialog; // Okno zawierające ustawiania programu
+
+	QToolButton *m_showLibraryButton;
+	QToolButton *m_showPlaylistsButton;
+	QToolButton *m_showFavoriteButton;
+	QToolButton *m_showAuthorsButton;
+	QToolButton *m_showAlbumsButton;
+
+	QToolButton *m_settingsButton; // Przycisk optwierający ustawienia programu
+};
+
+#endif // SIDEBAR_HPP
